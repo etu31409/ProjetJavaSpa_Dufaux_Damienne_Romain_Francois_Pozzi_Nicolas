1e257366a649b510a27de182f38d336903a15300
etu33783 <etu33783@henallux.be>
2018-04-28 18:47:14 +0200

    maj

--- src/main/java/dataAcessPackage/DBDAOMedicament.java
+++ src/main/java/dataAcessPackage/DBDAOMedicament.java
@@ -11,7 +11,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.GregorianCalendar;
 
-public class DBDAOMedicament {
+public class DBDAOMedicament implements IMedicament{
     private Connection connectionUnique;
     private String sqlInstruction;
 
