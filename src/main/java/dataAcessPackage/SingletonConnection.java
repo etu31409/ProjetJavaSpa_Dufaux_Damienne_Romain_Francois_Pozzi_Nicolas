cf53a6085eda5670a4ebbd6cf6236b577ab40c07
etu33783 <etu33783@henallux.be>
2018-04-28 15:41:40 +0200

    modifie le singleton et ajoute une exception

--- src/main/java/dataAcessPackage/SingletonConnection.java
+++ src/main/java/dataAcessPackage/SingletonConnection.java
@@ -1,18 +1,22 @@
 package dataAcessPackage;
 
+import exceptionPackage.*;
 import java.sql.*;
 import java.util.*;
 
 public class SingletonConnection {
     private static Connection connectionUnique;
 
-    public static Connection getUniqueInstance(){
+    public static Connection getUniqueInstance() throws SingletonConnectionException {
         if(connectionUnique == null){
             try{
                 connectionUnique = DriverManager.getConnection("jdbc:mysql://localhost:3306/spabd?useSSL=false",
-                        "root", "Pn783iOm");
+                        "root", "root");
                 // Créer l'instruction SQL avec ? pour éviter injection SQL
-            }catch (SQLException exception) { System.out.println(exception.getMessage()); }
+            }catch (SQLException exception)
+            {
+                throw new SingletonConnectionException();
+            }
         }
         return connectionUnique;
     }
